958ed58670cc712d7364596ce4a8286de058836e
CyberMergina <cyber.mergina@gmail.com>
2017-06-02 02:16:23 +0900

    Upgrade: メイン画面から一覧画面（仮置き）の作成と画面遷移

---
+++ app/src/main/java/com/example/cybermergina/myapplication/ListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cybermergina.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
